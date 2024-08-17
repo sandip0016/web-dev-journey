e847dfa001a926bc3eb548a30f2b2ec882bb3055
sandip0016 <gurungsandeep055@gmail.com>
2024-08-17 22:43:06 +0545

    if statement  in JS

---
+++ day12/app.js
@@ -0,0 +1,10 @@
+let color="green";
+if(color==="red"){
+    console.log("Stop! light color is red");
+}
+if(color==="yellow"){
+    console.log("Slow down. light color is yellow");
+}
+if(color==="green"){
+    console.log("go. light color is green");
+}
